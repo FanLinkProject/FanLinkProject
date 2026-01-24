a09a5c4ccbded97c897eb42f9e517820abfc252b
hye-eun-park <think_02@naver.com>
2026-01-24 17:38:49 +0900

    ✨ feat: PasswordEncoder 추가

--- backend/src/main/java/org/example/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/global/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -56,5 +58,11 @@ public class SecurityConfig {
             );
         
         return http.build();
+    }
+
+    // 비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
