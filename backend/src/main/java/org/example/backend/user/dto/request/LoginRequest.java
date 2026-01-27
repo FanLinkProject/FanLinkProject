895502dd261bb48fcd074edadae41d3405a63c15
tory <chkoreakr@google.com>
2026-01-27 14:16:05 +0900

    🛠️ refactor: dto 리팩토링

--- backend/src/main/java/org/example/backend/user/dto/request/LoginRequest.java
+++ backend/src/main/java/org/example/backend/user/dto/request/LoginRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 public record LoginRequest(
         @Email(message = "이메일 형식이 올바르지 않습니다.")
         @NotBlank(message = "이메일을 입력하세요.")
-        String Email,
+        String email,
 
         @NotBlank(message = "비밀번호를 입력하세요.")
         String password
