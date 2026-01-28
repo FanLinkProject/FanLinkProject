2cb18e20ebb3db39d630df7dca843d97dcb4a714
brunstart <brunstart@gmail.com>
2026-01-28 23:58:24 +0900

    feat : payment 엔티티 및 상태관리 ENUM

---
+++ backend/src/main/java/org/example/backend/payment/entity/Payment.java
@@ -0,0 +1,73 @@
+package org.example.backend.payment.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.payment.enums.PaymentMethod;
+import org.example.backend.payment.enums.PaymentStatus;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(name = "payment_key", unique = true)
+    private String paymentKey; // 토스 결제 키
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod method; // CARD, TOSS_PAY, ...
+
+    @Column(name = "paid_at")
+    private LocalDateTime paidAt;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public Payment(Long orderId, String paymentKey, BigDecimal amount, PaymentStatus status, PaymentMethod method,
+            LocalDateTime paidAt) {
+        this.orderId = orderId;
+        this.paymentKey = paymentKey;
+        this.amount = amount;
+        this.status = status;
+        this.method = method;
+        this.paidAt = paidAt;
+    }
+}
---
+++ backend/src/main/java/org/example/backend/payment/enums/PaymentStatus.java
@@ -0,0 +1,10 @@
+package org.example.backend.payment.enums;
+
+public enum PaymentStatus {
+    READY,
+    IN_PROGRESS,
+    DONE,
+    CANCELED,
+    ABORTED,
+    FAILED
+}
