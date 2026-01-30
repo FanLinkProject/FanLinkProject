31c11664086e5ceb07518d06e5f6f1df54b8be4c
yulb1 <kim482185@gmail.com>
2026-01-30 09:40:44 +0900

    🗑️ remove: dev에 있던 사용하지 않는 엔티티 제거

--- backend/src/main/java/org/example/backend/candy/entity/CandyType.java
+++
@@ -1,8 +0,0 @@
-package org.example.backend.candy.entity;
-
-public enum CandyType {
-    // 충전
-    CHARGE,
-    // 사용
-    USE
-}
--- backend/src/main/java/org/example/backend/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.backend.order.repository;
-
-import org.example.backend.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
