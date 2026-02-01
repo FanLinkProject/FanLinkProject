9dd8e30b01e83acffce804a6f64626509ce3ca29
brunstart <brunstart@gmail.com>
2026-02-01 14:06:51 +0900

    🛠️ refactor: ProductType 수정
    - 정산 및 사용 재화를 기준으로 상품 타입 4개로 구분

--- backend/src/main/java/org/example/backend/product/enums/ProductType.java
+++ backend/src/main/java/org/example/backend/product/enums/ProductType.java
@@ -1,7 +1,24 @@
 package org.example.backend.product.enums;
 
 public enum ProductType {
-    CANDY_CHARGE,
-    GOODS,
-    MEMBERSHIP
+    SETTLEMENT_CASH(true, ProductPaymentMethod.CASH_ONLY),
+    SETTLEMENT_CANDY(true, ProductPaymentMethod.CANDY_ONLY),
+    CASH(false, ProductPaymentMethod.CASH_ONLY),
+    CANDY(false, ProductPaymentMethod.CANDY_ONLY);
+
+    private final boolean isSettlementTarget;
+    private final ProductPaymentMethod paymentMethod;
+
+    ProductType(boolean isSettlementTarget, ProductPaymentMethod paymentMethod) {
+        this.isSettlementTarget = isSettlementTarget;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public boolean isSettlementTarget() {
+        return isSettlementTarget;
+    }
+
+    public ProductPaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
 }
