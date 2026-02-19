9be05b3d77f2160dba43d781501a27195bbba987
brunstart <brunstart@gmail.com>
2026-02-19 16:55:02 +0900

    🛠️ refactor: LocalDateTime -> Instant로 변경

--- backend/src/main/java/org/example/backend/comment/entity/Comment.java
+++ backend/src/main/java/org/example/backend/comment/entity/Comment.java
@@ -13,7 +13,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Entity
 @Getter
@@ -41,10 +41,10 @@ public class Comment {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @LastModifiedDate
-    private LocalDateTime updatedAt;
+    private Instant updatedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
